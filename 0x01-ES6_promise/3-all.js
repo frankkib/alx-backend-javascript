344af78780c707c2aa19bc6a09d2871e55282956
frankkib <frankkibagendi13@gmail.com>
2023-07-26 10:46:56 +0000

    using the A prototype to resolve all the promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
-      console.log(photoResponse.body);
-      console.log(userResponse.firstName);
-      console.log(userResponse.lastName);
+      console.log(photoResponse.body, userResponse.firstName, userResponse.lastName);
     })
     .catch((error) => {
       console.error('Signup system offline', error);
