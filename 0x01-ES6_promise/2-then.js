31cfe1ad71d1439266bc39eb4b3c68b24188450a
frankkib <frankkibagendi13@gmail.com>
2023-07-26 09:50:17 +0000

    writing a promise that returns an empty error with an object

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'Success' };
+    })
+    .catch((error) => {
+      console.error(error);
+      return new Error();
+    });
+}
