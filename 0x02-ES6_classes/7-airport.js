347f5fd808a0d678fe97ed802b91655afa2c727c
frankkib <frankkibagendi13@gmail.com>
2023-07-27 11:34:52 +0000

    Implementing a class named Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,34 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName === 'string') {
+      this._name = newName;
+    } else {
+      throw new Error('Name must be a string.');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode === 'string') {
+      this._code = newCode;
+    } else {
+      throw new Error('Code must be a string.');
+    }
+  }
+
+  toString() {
+    return this._code;
+  }
+}
