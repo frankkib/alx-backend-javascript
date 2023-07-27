3efb731a3aac09c05d5f44bb45dce7598ff4ffa5
frankkib <frankkibagendi13@gmail.com>
2023-07-27 10:41:59 +0000

    Function that declares a class and checkes the types also a static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amaount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
+    } else {
+      throw new Error('Amount must be a number.');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (typeof newCurrency === 'string') {
+      this._currency = newCurrency;
+    } else {
+      throw new Error('Currency must be a string.');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} ${this._currency.code}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRation === 'number') {
+      return amount * conversionRate;
+    }
+    throw new Error('Both must amount and conversionRate must be number');
+  }
+}
