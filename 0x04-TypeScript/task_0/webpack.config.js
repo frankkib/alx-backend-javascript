cad5cddb810ef7c6c310e802170cf7096b93db32
frankkib <frankkibagendi13@gmail.com>
2023-08-04 09:45:07 +0000

    fixed the configuration errors

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -5,6 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: "./js/main.ts",
   devtool: "inline-source-map",
   module: {
