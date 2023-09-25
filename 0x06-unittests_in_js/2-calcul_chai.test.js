6dddde134bf2a6c506aa555ceb5a744a2908eabd
frankkib <frankkibagend13@gmail.com>
2023-09-25 09:38:29 +0000

    Function using chai to test the scripts

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect
+const calculateNumber = require('./2-calcul_chai');
+
+
+describe('calculateNumber', function () {
+	it('should return sum of rounded numbers', function () {
+		expect(calculateNumber('SUM', 2.4, 3.7), 6);
+		expect(calculateNumber('SUM', 2.6 , 3.4), 6);
+	});
+
+	it('should subtract two rounded numbers', function () {
+		expect(calculateNumber('SUBTRACT', 6.4, 3), 3);
+		expect(calculateNumber('SUBTRACT', 7.6, 3), 5);
+	});
+
+	it('should divide two rounded numbers', function () {
+		expect(calculateNumber('DIVIDE', 8.8, 2), 4.5);
+		expect(calculateNumber('DIVIDE', 3, 0), 'Error');
+	});
+	it('should handle the invalid', function () {
+		expect(calculateNumber('INVALID', 2, 3), 'Invalid type');
+	});
+});
