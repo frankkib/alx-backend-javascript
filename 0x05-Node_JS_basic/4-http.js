1d4ecab5e79e316ef8b93deb23938a1a228a7700
frankkib <frankkibagend13@gmail.com>
2023-10-27 07:12:52 +0000

    serving an app using the node module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,18 @@
-const http = require('http')
+const http = require('http');
+
 
 const app = http.createServer((req, res) => {
-	res.writeHead(200, { 'Content-Type': 'text/plain' });
+  
+  res.setHeader('Content-Type', 'text/plain');
 
-	res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!\n');
 });
 
 
-app.listen(1245);
-
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
 
 module.exports = app;
+
