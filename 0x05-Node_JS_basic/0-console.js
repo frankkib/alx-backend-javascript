f7d5146a7b8dcf155a5091e6f600ce2f5fd5a9ab
frankkib <frankkibagend13@gmail.com>
2023-10-27 10:18:53 +0000

    fixed the eslint error

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,5 @@
-function displayMessage(message){
-	console.log(message);
+function displayMessage(message) {
+  console.log(message);
 }
 
-module.exports = displayMessage
-
+module.exports = displayMessage;
