b38a6b7dfa3ee1ebdfeb62fe5b11994c9dfdecfb
frankkib <frankkibagend13@gmail.com>
2023-10-27 11:06:15 +0000

    fixed the eslint error

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,13 +1,10 @@
 const http = require('http');
 
-
 const app = http.createServer((req, res) => {
-  
   res.setHeader('Content-Type', 'text/plain');
 
   res.end('Hello Holberton School!');
 });
-
 
 const port = 1245;
 app.listen(port, () => {
@@ -15,4 +12,3 @@ app.listen(port, () => {
 });
 
 module.exports = app;
-
