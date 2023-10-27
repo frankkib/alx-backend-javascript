038caf7ae5bf1b1e7abaad6fa96e83b5e3c2f186
frankkib <frankkibagend13@gmail.com>
2023-10-27 10:10:06 +0000

    express application

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,47 @@
+import { readDatabase } from '../utils';
+
+export class StudentsController {
+  static async getAllStudents(request, response) {
+    try {
+      const data = await readDatabase('./database.csv');
+      const fields = Object.keys(data);
+      const responseData = ['This is the list of our students'];
+
+      fields.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+      fields.forEach((field) => {
+        const studentsInField = data[field];
+        responseData.push(
+          `Number of students in ${field}: ${studentsInField.length}. List: ${studentsInField.join(', ')}`
+        );
+      });
+
+      response.status(200).send(responseData.join('\n'));
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(request, response) {
+    const { major } = request.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      response.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const data = await readDatabase('./database.csv');
+      const studentsInMajor = data[major];
+
+      if (studentsInMajor) {
+        response.status(200).send(`List: ${studentsInMajor.join(', ')}`);
+      } else {
+        response.status(200).send('No students found in the specified major');
+      }
+    } catch (error) {
+      response.status(500).send('Cannot load the database');
+    }
+  }
+}
+
---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const jsonData = JSON.parse(data);
+          resolve(jsonData);
+        } catch (parseError) {
+          reject(parseError);
+        }
+      }
+    });
+  });
+}
+
