3b73b00977ec663d037dbe5ac11e03087b8a4e8c
frankkib <frankkibagend13@gmail.com>
2023-10-27 07:51:08 +0000

    serving an app using express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+module.exports = app;
+
