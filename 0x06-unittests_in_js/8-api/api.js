a6950ff4cdb673eb3b77b723768a192bc26a5300
frankkib <frankkibagend13@gmail.com>
2023-10-30 09:16:14 +0000

    simple express app

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 7876;
+const port = 7865;
 
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
