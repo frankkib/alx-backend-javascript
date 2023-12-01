98d9b6895aa06f3ab8ab839a2658917d5e7f9e9a
frankkib <frankkibagend13@gmail.com>
2023-12-01 10:08:00 +0000

    simple express app

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,51 +1,27 @@
 const express = require('express');
-const fs = require('fs').promises;
-const csv = require('csv-parser');
+const countStudents = require('./3-read_file_async');
+
 const app = express();
-
-// Define a route for the root endpoint ("/")
-app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
-});
-
-// Define a route for the "/students" endpoint
-app.get('/students', async (req, res) => {
-  try {
-    const data = await fs.readFile('database.csv', 'utf8');
-    const students = {};
-    const fields = {};
-
-    const lines = data.trim().split('\n');
-    lines.forEach((line) => {
-      const [firstname, field] = line.split(',');
-
-      if (firstname && field) {
-        if (!students[field]) {
-          students[field] = 0;
-          fields[field] = [];
-        }
-
-        students[field]++;
-        fields[field].push(firstname);
-      }
-    });
-
-    const studentsList = {
-      students,
-      fields,
-    };
-
-    res.send(`This is the list of our students\n${JSON.stringify(studentsList, null, 2)}`);
-  } catch (error) {
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Start the server on port 1245
 const port = 1245;
+
+app.get('/', (req, res) => {
+  res.type('text/plain').send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const databasePath = req.query.database || 'database.csv';
+
+  countStudents(databasePath)
+    .then((output) => {
+      res.type('text/plain').send(`This is the list of our students\n${output}`);
+    })
+    .catch((error) => {
+      res.status(500).type('text/plain').send(`Internal Server Error: ${error.message}`);
+    });
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}/`);
 });
 
 module.exports = app;
-
