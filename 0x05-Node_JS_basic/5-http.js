82b75c1e9cf85febea9c1ec2a4768e944fd9ac3e
frankkib <frankkibagend13@gmail.com>
2023-12-01 09:39:06 +0000

    simple app for counting number of students in the database

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+
+  if (parsedUrl.pathname === '/') {
+    // Handle the / route
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!\n');
+  } else if (parsedUrl.pathname === '/students') {
+    // Handle the /students route
+    const databasePath = parsedUrl.query.database || 'database.csv';
+
+    countStudents(databasePath)
+      .then((output) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${output}`);
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(`Internal Server Error: ${error.message}`);
+      });
+  } else {
+    // Handle unknown routes
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}/`);
+});
+
+module.exports = app;
