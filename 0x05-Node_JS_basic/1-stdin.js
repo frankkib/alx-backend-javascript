637b8c8e64eb97efca2c7e679360073c226e7c67
frankkib <frankkibagendi13@gmail.com>
2024-09-24 13:36:22 +0300

    Test executes correctly

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,11 +5,11 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-console.log('Welcome to Holberton School, what is your name?\n');
+console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
   const name = input.trim();
-  console.log(`Your name is: ${name}\n`);
-  console.log('This important software is now closing\n');
+  console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   rl.close();
 });
